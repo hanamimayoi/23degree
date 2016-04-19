997b3067f6970bebbf0fc4d20d381cc0631e33f4
dymlangman <dymlangman@163.com>
2016-04-19 20:45:32 +0800

    修改了SplashActivity，等待MainActivity的接入-----By Deng

--- app/src/main/java/com/quintet/littleweather/activity/SplashActivity.java
+++ app/src/main/java/com/quintet/littleweather/activity/SplashActivity.java
@@ -1,15 +1,109 @@
 package com.quintet.littleweather.activity;
 
+import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.os.Message;
 
 import com.quintet.littleweather.R;
 
-public class SplashActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SplashActivity extends Activity {
+
+
+    public static final int MSG_TIME_OUT = 1;
+    public static final String DB_NAME = "china_city.db"; //数据库的名字
+    public static final String PACKAGE_NAME = "com.quintet.littleweather"; //包名
+    public static final String DB_PATH = "data/data/" + PACKAGE_NAME + "/" + DB_NAME;//db存放的路径
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //在Splash界面等待三秒进入MainActivity
+        waitWhile();
+
+
+        //将放在assets文件夹里面的db数据库文件，copy到data/data/包名目录下
+        copyDBToData();
     }
+
+
+    private void copyDBToData() {
+        File db = new File(DB_PATH);
+        if (db.exists()) {
+            return;
+        } else {
+
+
+            try {
+                AssetManager assets = getAssets();
+
+                InputStream is = assets.open(DB_NAME);
+
+                FileOutputStream fos = new FileOutputStream(db);
+
+                byte[] buf = new byte[1024];
+                int len = 0;
+
+                while ((len = is.read(buf)) != -1) {
+                    fos.write(buf, 0, len);
+                }
+
+                is.close();
+                fos.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+
+    private void waitWhile() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Message msg = myHandler.obtainMessage();
+                msg.what = MSG_TIME_OUT;
+                myHandler.sendMessage(msg);
+
+
+            }
+        }).start();
+    }
+
+    Handler myHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            switch (msg.what) {
+                case MSG_TIME_OUT:
+                    //enterMainActivity();
+            }
+        }
+    };
+
+
+    //等待MainActivity 写好后，进行耦合。。。
+/*    private void enterMainActivity() {
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }*/
 }
