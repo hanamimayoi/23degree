6e9393fd1532805d9b57a41b7f2d176e08078939
hanami <hanami0625@163.com>
2016-04-25 11:18:32 +0800

    修改了关于界面的状态栏

--- app/src/main/java/com/quintet/littleweather/activity/AboutActivity.java
+++ app/src/main/java/com/quintet/littleweather/activity/AboutActivity.java
@@ -1,14 +1,14 @@
 package com.quintet.littleweather.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.quintet.littleweather.R;
+import com.quintet.littleweather.base.BaseActivity;
 import com.quintet.littleweather.fragment.AboutFragment;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends BaseActivity {
 
     private Toolbar mToolbar;
 
@@ -16,7 +16,7 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
+        setStatusBarColor(R.color.colorPrimary);
         getFragmentManager().beginTransaction().add(R.id.frame_layout, new AboutFragment()).commit();
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
